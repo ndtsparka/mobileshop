2042f26a60eb65164a732bde7db8de43fea0303f
cuongtq <tranquangcuong97@gmail.com>
2020-10-19 20:28:01 +0700

    a

--- home.js
+++ home.js
@@ -11,10 +11,22 @@ let iphone =[{
     'img':'./img/iphone-8-plus-128gb-082720-052722-400x460.png'
 },
 {
+    'name':'IPHONE 11 64GB',
+    'price':19.990.000đ,
+    'description':'Ưu đãi khủng đến 2 triệu khi đặt sớm',
+    'img':'./img/iphone-11-red-2-400x460-400x460.png'
+},
+{
     'name':'IPHONE 11 128GB',
     'price':21.990.000đ,
     'description':'khuyễn mãi trị giá 1.000.000đ',
     'img':'./img/iphone-11-128gb-green-400x460.png'
+},
+{
+    'name':'IPHONE Xs 64GB',
+    'price':17.990.000đ,
+    'description':'Ưu đãi khủng đến 2 triệu khi đặt sớm',
+    'img':'./img/iphone-xs-gold-400x460-1-400x460.png'
 },
 
 {
@@ -24,16 +36,70 @@ let iphone =[{
     'img':'./img/iphone-xs-gold-400x460-1-400x460.png'
 },
 {
-    'name':'IPHONE Xs 64GB',
-    'price':17.990.000đ,
-    'description':'Ưu đãi khủng đến 2 triệu khi đặt sớm',
-    'img':'./img/iphone-xs-gold-400x460-1-400x460.png'
+    'name':'OPPO A12',
+    'price':2.990.000đ,
+    'description':'Giảm 5% khi đặt online',
+    'img':'./img/oppo-a12-xanh-400x460-400x460.png'
+},{
+    'name':'OPPO A31',
+    'price':4.490.000đ,
+    'description':'Quà tặng lên tới 500.000đ',
+    'img':'./img/oppo-a31-4gb-128gb-trang-400x460-400x460.png'
+},{
+    'name':'OPPO A93',
+    'price':7.490.000đ,
+    'description':'Giảm giá lên tới 5%',
+    'img':'oppo-a93-230520-060502-400x460.png'
+},{
+    'name':'OPPO A53',
+    'price':4.990.000đ,
+    'description':'Quà tặng trị lên tới 500.000đ,
+    'img':'./img/oppo-a53-2020-blue-400x460-400x460.png'
 },
 {
-    'name':'IPHONE 11 64GB',
-    'price':19.990.000đ,
-    'description':'Ưu đãi khủng đến 2 triệu khi đặt sớm',
+    'name':'OPPO RENO4',
+    'price':8.490.000đ,
+    'description':'Quà tặng trị giá lên tới 5000.000đ',
     'img':'./img/iphone-11-red-2-400x460-400x460.png'
+},
+{
+    'name':'SAMSUNG GALAXY A21S',
+    'price':4.390.000Đ,
+    'description':'Quà tặng 300.000đ',
+    'img':'./img/samsung-galaxy-a21s-3gb-055520-045516-400x460.png'
+},
+{
+    'name':'SAMSUNG GALAXY A31',
+    'price':6.090.000đ,
+    'description':'Quà tặng 300.000đ',
+    'img':'./img/samsung-galaxy-a31-400x460-white-400x460.png-11-red-2-400x460-400x460.png'
+},
+{
+    'name':'SAMSUNG GALAXY NOTE20 ULTRA',
+    'price':29.990.000đ,
+    'description':'Ưu đãi khủng đến 3 triệu khi đặt sớm',
+    'img':'./img/samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png'
+},
+{
+    'name':'SAMSUNG GALAXY NOTE20',
+    'price':23.990.000đ,
+    'description':'Giảm giá 3.000.000đ qua quà tặng Galaxy',
+    'img':'./img/samsung-galaxy-note-20-xanhla-400x460-400x460.png'
+},
+{
+    'name':'SAMSUNG GALAXY S20 FE',
+    'price':15.990.000đ,
+    'description':'Giảm giá 2.000.000đ qua quà tặng Galaxy',
+    'img':'./img/samsung-galaxy-note-20-xanhla-400x460-400x460.png'
+},
+{
+    'name':'SAMSUNG GALAXY Z-FOLD',
+    'price':50.00.000đ,
+    'description':'Giảm giá 2.000.000đ qua quà tặng Galaxy',
+    'img':'./img/samsung-galaxy-note-20-xanhla-400x460-400x460.png'
 },
 ]
 
+function display(){
+    
+}
