95dcc319f14bf2f8635ed9f8d42b15eeb2292ec0
cuongtq <tranquangcuong97@gmail.com>
2020-10-21 02:15:22 +0700

    s

--- home.js
+++ home.js
@@ -2,105 +2,202 @@ let products = [{
     'name': 'IPHONE 7 PLUS',
     'price': '10.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
-    'img': './img/iphone-xs-gold-400x460-1-400x460.png'
+    'img': './img/iphone-xs-gold-400x460-1-400x460.png',
+    'brand':'iphone'
 },
 {
     'name': 'IPHONE 8 PLUS',
     'price': '14.990.000đ',
     'description': 'Giảm thêm 1.000.000đ',
-    'img': './img/iphone-8-plus-128gb-082720-052722-400x460.png'
+    'img': './img/iphone-8-plus-128gb-082720-052722-400x460.png',
+    'brand':'iphone'
 },
+{
+    'name': 'IPHONE 7 GOLD',
+    'price': '12.990.000đ',
+    'description': 'Giảm thêm 1.000.000đ',
+    'img': './img/iphone-7-gold-600x600.jpg',
+    'brand':'iphone'
+},
+{
+    'name': 'IPHONE 8 PLUS 128GB',
+    'price': '18.990.000đ',
+    'description': 'Giảm thêm 1.000.000đ',
+    'img': './img/iphone-8-plus-128gb.jpg',
+    'brand':'iphone'
+},
+{
+    'name': 'IPHONE 11 PROMAX 512GB',
+    'price': '32.990.000đ',
+    'description': 'Quà tặng trị giá 4.000.000đ',
+    'img': './img/iphone-11-pro-max-512gb-gold-600x600.jpg',
+    'brand':'iphone'
+},
+{
+    'name': 'IPHONE 12 MINI',
+    'price': '34.990.000đ',
+    'description': 'Quà tặng trị giá 2.000.000đ',
+    'img': './img/iphone-12-mini.jpg',
+    'brand':'iphone'
+},
+
 {
     'name': 'IPHONE 11 64GB',
     'price': '19.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
-    'img': './img/iphone-11-red-2-400x460-400x460.png'
+    'img': './img/iphone-11-red-2-400x460-400x460.png',
+    'brand':'iphone'
 },
 {
     'name': 'IPHONE 11 128GB',
     'price': '21.990.000đ',
     'description': 'khuyễn mãi trị giá 1.000.000đ',
-    'img': './img/iphone-11-128gb-green-400x460.png'
+    'img': './img/iphone-11-128gb-green-400x460.png',
+    'brand':'iphone'
 },
 {
     'name': 'IPHONE Xs 64GB',
     'price': '17.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
-    'img': './img/iphone-xs-gold-400x460-1-400x460.png'
+    'img': './img/iphone-xs-gold-400x460-1-400x460.png',
+    'brand':'iphone'
 },
 
 {
     'name': 'IPHONE 11 PROMAX 512GB',
     'price': '38.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
-    'img': './img/iphone-xs-gold-400x460-1-400x460.png'
+    'img': './img/iphone-xs-gold-400x460-1-400x460.png',
+    'brand':'iphone'
+},
+{
+    'name': 'IPHONE 12 PROMAX 256GB',
+    'price': '38.990.000đ',
+    'description': 'Quà tặng trị giá 2.000.000đ',
+    'img': './img/iphone-12-pro-max-256gb.jpg',
+    'brand':'iphone'
+},
+{
+    'name': 'IPHONE SE 128GB',
+    'price': '33.990.000đ',
+    'description': 'Quà tặng trị giá 2.000.000đ',
+    'img': './img/iphone-12-pro-max-256gb.jpg',
+    'brand':'iphone'
 },
 {
     'name': 'OPPO A12',
     'price': '2.990.000đ',
     'description': 'Giảm 5% khi đặt online',
-    'img': './img/oppo-a12-xanh-400x460-400x460.png'
-}, {
+    'img': './img/oppo-a12-xanh-400x460-400x460.png',
+    'brand':'oppo'
+},
+ {
     'name': 'OPPO A31',
     'price': '4.490.000đ',
     'description': 'Quà tặng lên tới 500.000đ',
-    'img': './img/oppo-a31-4gb-128gb-trang-400x460-400x460.png'
+    'img': './img/oppo-a31-4gb-128gb-trang-400x460-400x460.png',
+    'brand':'oppo'
 }, {
     'name': 'OPPO A93',
     'price': '7.490.000đ',
     'description': 'Giảm giá lên tới 5%',
-    'img': './img/oppo-a93-230520-060502-400x460.png'
-}, {
+    'img': './img/oppo-a93-230520-060502-400x460.png',
+    'brand':'oppo'
+}, 
+{
     'name': 'OPPO A53',
     'price': '4.990.000đ',
     'description': 'Quà tặng trị lên tới 500.000đ',
-    'img': './img/oppo-a53-2020-blue-400x460-400x460.png'
+    'img': './img/oppo-a53-2020-blue-400x460-400x460.png',
+    'brand':'oppo'
+},
+{
+    'name': 'OPPO FIND X2',
+    'price': '19.990.000đ',
+    'description': 'Quà tặng trị lên tới 2.000.000đ',
+    'img': './img/oppo-findx.jpg',
+    'brand':'oppo'
 },
 {
     'name': 'OPPO RENO4',
     'price': '8.490.000đ',
-    'description': 'Quà tặng trị giá lên tới 5000.000đ',
-    'img': './img/iphone-11-red-2-400x460-400x460.png'
+    'description': 'Quà tặng trị giá lên tới 500.000đ',
+    'img': './img/oppo-reno4-260720-040737-400x460.png',
+    'brand':'oppo'
+},
+{
+    'name': 'OPPO A9',
+    'price': '5.990.000đ',
+    'description': 'Giảm giá thêm 5%',
+    'img': './img/oppo-a9.jpg',
+    'brand':'oppo'
+},
+{
+    'name': 'OPPO A52',
+    'price': '5.690.000đ',
+    'description': 'Quà tặng trị giá lên tới 500.000đ',
+    'img': './img/oppo-A52.jpg',
+    'brand':'oppo'
 },
 {
     'name': 'SAMSUNG GALAXY A21S',
     'price': '4.390.000Đ',
     'description': 'Quà tặng 300.000đ',
-    'img': './img/samsung-galaxy-a21s-3gb-055520-045516-400x460.png'
+    'img': './img/samsung-galaxy-a21s-3gb-055520-045516-400x460.png',
+    'brand':'samsung'
 },
 {
     'name': 'SAMSUNG GALAXY A31',
     'price': '6.090.000đ',
     'description': 'Quà tặng 300.000đ',
-    'img': './img/samsung-galaxy-a31.png'
+    'img': './img/samsung-galaxy-a31.png',
+    'brand':'samsung'
 },
 {
     'name': 'SAMSUNG GALAXY NOTE20 ULTRA',
     'price': '29.990.000đ',
     'description': 'Ưu đãi khủng đến 3 triệu khi đặt sớm',
-    'img': './img/samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png'
+    'img': './img/samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png',
+    'brand':'samsung'
+},
+{
+    'name': 'SAMSUNG GALAXY A71',
+    'price': '9.990.000đ',
+    'description': 'Giảm giá thêm 500.000đ',
+    'img': './img/samsung-galaxy-a71.jpg',
+    'brand':'samsung'
 },
 {
     'name': 'SAMSUNG GALAXY NOTE20',
     'price': '23.990.000đ',
     'description': 'Giảm giá 3.000.000đ qua quà tặng Galaxy',
-    'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png'
+    'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
+    'brand':'samsung'
 },
 {
     'name': 'SAMSUNG GALAXY S20 FE',
     'price': '15.990.000đ',
     'description': 'Giảm giá 2.000.000đ qua quà tặng Galaxy',
-    'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png'
+    'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
+    'brand':'samsung'
+},
+{
+    'name': 'SAMSUNG GALAXY A71',
+    'price': '6.290.000đ',
+    'description': 'Giảm giá thêm 300.000đ',
+    'img': './img/samsung-galaxy-a30s.jpg',
+    'brand':'samsung'
 },
 {
     'name': 'SAMSUNG GALAXY Z-FOLD',
     'price': '50.00.000đ',
     'description': 'Giảm giá 2.000.000đ qua quà tặng Galaxy',
-    'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png'
+    'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
+    'brand':'samsung'
 },
 ]
 
-function display(listProduct) {
+function display() {
     let domProducts = document.getElementById('products');
     domProducts.innerHTML = '';
     
@@ -129,4 +226,142 @@ function display(listProduct) {
     }
 
 }
-display(products);
+display();
+
+
+function changedisplay(type,element){
+   let tabs = document.getElementsByClassName('tab-item');
+   for(let i=0; i<tabs.length;i++){
+       tabs[i].style.background = '#183544';
+   }
+   element.style.background = 'tomato';
+   document.getElementById(type).style.display = 'flex';
+   switch (type){
+       case 'all':
+           document.getElementById('iphone').style.display = 'none';
+           document.getElementById('samsung').style.display = 'none';
+           document.getElementById('oppo').style.display = 'none';
+           break;
+       case 'iphone':
+        document.getElementById('all').style.display = 'none';
+        document.getElementById('oppo').style.display = 'none';
+        document.getElementById('samsung').style.display = 'none';
+        break;
+       case 'samsung':
+        document.getElementById('all').style.display = 'none';
+        document.getElementById('oppo').style.display = 'none';
+        document.getElementById('iphone').style.display = 'none';
+        break;
+       case 'oppo':
+        document.getElementById('all').style.display = 'none';
+        document.getElementById('iphone').style.display = 'none';
+        document.getElementById('samsung').style.display = 'none';
+        break;
+        
+
+   }
+}
+
+
+
+function iphone() {
+    let listiphone = document.getElementById('iphone');
+    listiphone.innerHTML = '';
+    
+    for (const iphone of products) {
+       if(iphone.brand==='iphone'){
+        let info = `
+      <div class="content">
+            <div class="content-img">
+                <img src="${iphone.img}" alt=""  width="240px" height="240px">
+            </div>
+            <div class="content-name">
+                <h4>
+                ${iphone.name}
+                </h4>
+            </div>
+            <div class="content-price">
+                  <h4>
+                     ${iphone.price}
+                  </h4>
+            </div>
+            <div class = "content-descripton">
+                <p>${iphone.description}</p>
+            <div>
+      </div>
+      `
+       listiphone.innerHTML += info;
+       }
+       
+    }
+
+}
+iphone();
+
+function oppo() {
+    let listiphone = document.getElementById('oppo');
+    listiphone.innerHTML = '';
+    
+    for (const phone of products) {
+       if(phone.brand==='oppo'){
+        let oppo = `
+      <div class="content">
+            <div class="content-img">
+                <img src="${phone.img}" alt=""  width="240px" height="240px">
+            </div>
+            <div class="content-name">
+                <h4>
+                ${phone.name}
+                </h4>
+            </div>
+            <div class="content-price">
+                  <h4>
+                     ${phone.price}
+                  </h4>
+            </div>
+            <div class = "content-descripton">
+                <p>${phone.description}</p>
+            <div>
+      </div>
+      `
+       listiphone.innerHTML += oppo;
+       }
+       
+    }
+
+}
+oppo();
+
+function samsung() {
+    let listiphone = document.getElementById('samsung');
+    listiphone.innerHTML = '';
+    
+    for (const phone of products) {
+       if(phone.brand==='samsung'){
+        let samsung = `
+      <div class="content">
+            <div class="content-img">
+                <img src="${phone.img}" alt=""  width="240px" height="240px">
+            </div>
+            <div class="content-name">
+                <h4>
+                ${phone.name}
+                </h4>
+            </div>
+            <div class="content-price">
+                  <h4>
+                     ${phone.price}
+                  </h4>
+            </div>
+            <div class = "content-descripton">
+                <p>${phone.description}</p>
+            <div>
+      </div>
+      `
+       listiphone.innerHTML += samsung;
+       }
+       
+    }
+
+}
+samsung();
