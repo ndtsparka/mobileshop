c353c8c49976167e036a8910455da6ae01378358
cuongtq <tranquangcuong97@gmail.com>
2020-10-25 11:49:16 +0700

    a

--- home.js
+++ home.js
@@ -3,42 +3,42 @@ let products = [{
     'price': '10.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-xs-gold-400x460-1-400x460.png',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE 8 PLUS',
     'price': '14.990.000đ',
     'description': 'Giảm thêm 1.000.000đ',
     'img': './img/iphone-8-plus-128gb-082720-052722-400x460.png',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE 7 GOLD',
     'price': '12.990.000đ',
     'description': 'Giảm thêm 1.000.000đ',
     'img': './img/iphone-7-gold-600x600.jpg',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE 8 PLUS 128GB',
     'price': '18.990.000đ',
     'description': 'Giảm thêm 1.000.000đ',
     'img': './img/iphone-8-plus-128gb.jpg',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE 11 PROMAX 512GB',
     'price': '32.990.000đ',
     'description': 'Quà tặng trị giá 4.000.000đ',
     'img': './img/iphone-11-pro-max-512gb-gold-600x600.jpg',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE 12 MINI',
     'price': '34.990.000đ',
     'description': 'Quà tặng trị giá 2.000.000đ',
     'img': './img/iphone-12-mini.jpg',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 
 {
@@ -46,21 +46,21 @@ let products = [{
     'price': '19.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-11-red-2-400x460-400x460.png',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE 11 128GB',
     'price': '21.990.000đ',
     'description': 'khuyễn mãi trị giá 1.000.000đ',
     'img': './img/iphone-11-128gb-green-400x460.png',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE XS 64GB',
     'price': '17.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-xs-gold-400x460-1-400x460.png',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 
 {
@@ -68,55 +68,55 @@ let products = [{
     'price': '38.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-xs-gold-400x460-1-400x460.png',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE 12 PROMAX 256GB',
     'price': '38.990.000đ',
     'description': 'Quà tặng trị giá 2.000.000đ',
     'img': './img/iphone-12-pro-max-256gb.jpg',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'IPHONE SE 128GB',
     'price': '33.990.000đ',
     'description': 'Quà tặng trị giá 2.000.000đ',
     'img': './img/iphone-12-pro-max-256gb.jpg',
-    'brand':'iphone'
+    'brand':'IPHONE'
 },
 {
     'name': 'OPPO A12',
     'price': '2.990.000đ',
     'description': 'Giảm 5% khi đặt online',
     'img': './img/oppo-a12-xanh-400x460-400x460.png',
-    'brand':'oppo'
+    'brand':'OPPO'
 },
  {
     'name': 'OPPO A31',
     'price': '4.490.000đ',
     'description': 'Quà tặng lên tới 500.000đ',
     'img': './img/oppo-a31-4gb-128gb-trang-400x460-400x460.png',
-    'brand':'oppo'
+    'brand':'OPPO'
 }, {
     'name': 'OPPO A93',
     'price': '7.490.000đ',
     'description': 'Giảm giá lên tới 5%',
     'img': './img/oppo-a93-230520-060502-400x460.png',
-    'brand':'oppo'
+    'brand':'OPPO'
 }, 
 {
     'name': 'OPPO A53',
     'price': '4.990.000đ',
     'description': 'Quà tặng trị lên tới 500.000đ',
     'img': './img/oppo-a53-2020-blue-400x460-400x460.png',
-    'brand':'oppo'
+    'brand':'OPPO'
 },
 {
     'name': 'OPPO FIND X2',
     'price': '19.990.000đ',
     'description': 'Quà tặng trị lên tới 2.000.000đ',
     'img': './img/oppo-findx.jpg',
-    'brand':'oppo'
+    'brand':'OPPO'
 },
 {
     'name': 'OPPO RENO 4',
@@ -124,35 +124,35 @@ let products = [{
     'description': 'Quà tặng trị giá lên tới 500.000đ',
     'img': './img/oppo-reno4-260720-040737-400x460.png',
     'imgsub':'./img/oppo-reno4-hot.jpg',
-    'brand':'oppo'
+    'brand':'OPPO'
 },
 {
     'name': 'OPPO A9',
     'price': '5.990.000đ',
     'description': 'Giảm giá thêm 5%',
     'img': './img/oppo-a9.jpg',
-    'brand':'oppo'
+    'brand':'OPPO'
 },
 {
     'name': 'OPPO A52',
     'price': '5.690.000đ',
     'description': 'Quà tặng trị giá lên tới 500.000đ',
     'img': './img/oppo-A52.jpg',
-    'brand':'oppo'
+    'brand':'OPPO'
 },
 {
     'name': 'SAMSUNG GALAXY A21S',
     'price': '4.390.000Đ',
     'description': 'Quà tặng 300.000đ',
     'img': './img/samsung-galaxy-a21s-3gb-055520-045516-400x460.png',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 {
     'name': 'SAMSUNG GALAXY A31',
     'price': '6.090.000đ',
     'description': 'Quà tặng 300.000đ',
     'img': './img/samsung-galaxy-a31.png',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 {
     'name': 'SAMSUNG GALAXY NOTE20 ULTRA',
@@ -160,42 +160,42 @@ let products = [{
     'description': 'Ưu đãi khủng đến 3 triệu khi đặt sớm',
     'img': './img/samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png',
     'imgsub':'./img/samsung-galaxy-note-20-ultra-hot.jpg',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 {
     'name': 'SAMSUNG GALAXY A71',
     'price': '9.990.000đ',
     'description': 'Giảm giá thêm 500.000đ',
     'img': './img/samsung-galaxy-a71.jpg',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 {
     'name': 'SAMSUNG GALAXY NOTE20',
     'price': '23.990.000đ',
     'description': 'Giảm giá 3.000.000đ qua quà tặng Galaxy',
     'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 {
     'name': 'SAMSUNG GALAXY S20 FE',
     'price': '15.990.000đ',
     'description': 'Giảm giá 2.000.000đ qua quà tặng Galaxy',
     'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 {
     'name': 'SAMSUNG GALAXY A30S',
     'price': '6.290.000đ',
     'description': 'Giảm giá thêm 300.000đ',
     'img': './img/samsung-galaxy-a30s.jpg',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 {
     'name': 'SAMSUNG GALAXY Z-FOLD',
     'price': '50.00.000đ',
     'description': 'Giảm giá 2.000.000đ qua quà tặng Galaxy',
     'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
-    'brand':'samsung'
+    'brand':'SAMSUNG'
 },
 ]
 
@@ -207,7 +207,7 @@ function display() {
         let info = `
       <div class="content">
             <div class="content-img">
-                <img src="${product.img}" alt=""  width="240px" height="240px">
+                <img src="${product.img}" alt=""  width="240px" height="240px" onclick="viewDetail('${product.name}')">
             </div>
             <div class="content-name">
                 <h4 onclick="viewDetail('${product.name}')">
@@ -285,25 +285,25 @@ function iphone() {
     let listiphone = document.getElementById('iphone');
     listiphone.innerHTML = '';
     
-    for (const iphone of products) {
-       if(iphone.brand==='iphone'){
+    for (const product of products) {
+       if(product.brand==='IPHONE'){
         let info = `
       <div class="content">
             <div class="content-img">
-                <img src="${iphone.img}" alt=""  width="240px" height="240px">
+                <img src="${product.img}" alt=""  width="240px" height="240px" onclick="viewDetail('${product.name}')">
             </div>
             <div class="content-name">
-                <h4>
-                ${iphone.name}
+                <h4 onclick="viewDetail('${product.name}')">
+                ${product.name}
                 </h4>
             </div>
             <div class="content-price">
                   <h4>
-                     ${iphone.price}
+                     ${product.price}
                   </h4>
             </div>
             <div class = "content-descripton">
-                <p>${iphone.description}</p>
+                <p>${product.description}</p>
             <div>
       </div>
       `
@@ -319,25 +319,25 @@ function oppo() {
     let listiphone = document.getElementById('oppo');
     listiphone.innerHTML = '';
     
-    for (const phone of products) {
-       if(phone.brand==='oppo'){
+    for (const product of products) {
+       if(product.brand==='OPPO'){
         let oppo = `
       <div class="content">
             <div class="content-img">
-                <img src="${phone.img}" alt=""  width="240px" height="240px">
+                <img src="${product.img}" alt=""  width="240px" height="240px" onclick="viewDetail('${product.name}')">
             </div>
             <div class="content-name">
-                <h4>
-                ${phone.name}
+                <h4 onclick="viewDetail('${product.name}')">
+                ${product.name}
                 </h4>
             </div>
             <div class="content-price">
                   <h4>
-                     ${phone.price}
+                     ${product.price}
                   </h4>
             </div>
             <div class = "content-descripton">
-                <p>${phone.description}</p>
+                <p>${product.description}</p>
             <div>
       </div>
       `
@@ -353,25 +353,25 @@ function samsung() {
     let listiphone = document.getElementById('samsung');
     listiphone.innerHTML = '';
     
-    for (const phone of products) {
-       if(phone.brand==='samsung'){
+    for (const product of products) {
+       if(product.brand==='SAMSUNG'){
         let samsung = `
       <div class="content">
             <div class="content-img">
-                <img src="${phone.img}" alt=""  width="240px" height="240px">
+                <img src="${product.img}" alt=""  width="240px" height="240px" onclick="viewDetail('${product.name}')">
             </div>
             <div class="content-name">
-                <h4>
-                ${phone.name}
+                <h4 onclick="viewDetail('${product.name}')">
+                ${product.name}
                 </h4>
             </div>
             <div class="content-price">
                   <h4>
-                     ${phone.price}
+                     ${product.price}
                   </h4>
             </div>
             <div class = "content-descripton">
-                <p>${phone.description}</p>
+                <p>${product.description}</p>
             <div>
       </div>
       `
@@ -390,10 +390,10 @@ function displaysearchphone(phones){
         let search = `
       <div class="content-search">
             <div class="content-img">
-                <img src="${product.img}" alt=""  width="240px" height="240px">
+                <img src="${product.img}" alt=""  width="240px" height="240px" onclick="viewDetail('${product.name}')">
             </div>
             <div class="content-name">
-                <h4>
+                <h4 onclick="viewDetail('${product.name}')">
                 ${product.name}
                 </h4>
             </div>
@@ -445,49 +445,49 @@ function displayhotitem(){
        phoneshot.innerHTML = '';
     let hotitem = `
      <li class="not-change">
-        <img src="${products[22].imgsub}" class="content-img" >
-        <div class="content-name"><h4>${products[22].name}</h4></div>
+        <img src="${products[22].imgsub}" class="content-img" onclick="viewDetail('${products[22].name}')">
+        <div class="content-name"><h4 onclick="viewDetail('${products[22].name}')">${products[22].name}</h4></div>
         <div class="content-price"><h4>${products[22].price}</h4></div>
         <div class="content-descripton"><p>${products[22].description}</b></p></div>
     </li>
     <li class="change">
-        <img src="${products[19].img}" class="content-img" >
+        <img src="${products[19].img}" class="content-img"onclick="viewDetail('${products[19].name}')" >
         <div class="content-name"><h4 onclick="viewDetail('${products[19].name}')">${products[19].name}</h4></div>
         <div class="content-price"><h4>${products[19].price}</h4></div>
         <div class="content-descripton"><p>${products[19].description}</b></p></div>
     </li>
     <li class="change">
-        <img src="${products[14].img}" class="content-img" >
-        <div class="content-name"><h4>${products[14].name}</h4></div>
+        <img src="${products[14].img}" class="content-img" onclick="viewDetail('${products[14].name}')">
+        <div class="content-name"><h4 onclick="viewDetail('${products[14].name}')">${products[14].name}</h4></div>
         <div class="content-price"><h4>${products[14].price}</h4></div>
         <div class="content-descripton"><p>${products[14].description}</b></p></div>
     </li>
     <li class="change">
-        <img src="${products[19].img}" class="content-img" >
-        <div class="content-name"><h4>${products[4].name}</h4></div>
+        <img src="${products[19].img}" class="content-img" onclick="viewDetail('${products[4].name}')">
+        <div class="content-name"><h4 onclick="viewDetail('${products[4].name}')">${products[4].name}</h4></div>
         <div class="content-price"><h4>${products[4].price}</h4></div>
         <div class="content-descripton"><p>${products[4].description}</b></p></div>
     </li><li class="not-change">
-        <img src="${products[17].imgsub}" class="content-img" >
-        <div class="content-name"><h4>${products[17].name}</h4></div>
+        <img src="${products[17].imgsub}" class="content-img" onclick="viewDetail('${products[17].name}')">
+        <div class="content-name"><h4 onclick="viewDetail('${products[17].name}')">${products[17].name}</h4></div>
         <div class="content-price"><h4>${products[17].price}</h4></div>
         <div class="content-descripton"><p>${products[17].description}</b></p></div>
     </li>
     <li class="change">
-        <img src="${products[26].img}" class="content-img" >
-        <div class="content-name"><h4>${products[26].name}</h4></div>
+        <img src="${products[26].img}" class="content-img" onclick="viewDetail('${products[26].name}')">
+        <div class="content-name"><h4 onclick="viewDetail('${products[26].name}')">${products[26].name}</h4></div>
         <div class="content-price"><h4>${products[26].price}</h4></div>
         <div class="content-descripton"><p>${products[26].description}</b></p></div>
     </li>
     <li class="change">
-        <img src="${products[16].img}" class="content-img" >
-        <div class="content-name"><h4>${products[16].name}</h4></div>
+        <img src="${products[16].img}" class="content-img" onclick="viewDetail('${products[16].name}')">
+        <div class="content-name"><h4 onclick="viewDetail('${products[16].name}')">${products[16].name}</h4></div>
         <div class="content-price"><h4>${products[16].price}</h4></div>
         <div class="content-descripton"><p>${products[16].description}</b></p></div>
     </li>
     <li class="change">
-        <img src="${products[11].img}" class="content-img" >
-        <div class="content-name"><h4>${products[11].name}</h4></div>
+        <img src="${products[11].img}" class="content-img" onclick="viewDetail('${products[11].name}')">
+        <div class="content-name"><h4 onclick="viewDetail('${products[11].name}')">${products[11].name}</h4></div>
         <div class="content-price"><h4>${products[11].price}</h4></div>
         <div class="content-descripton"><p>${products[11].description}</b></p></div>
     </li>
