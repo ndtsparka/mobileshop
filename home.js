62cde0a5676f0ec7376270f9bb9ba65e1967e6db
Nguyen Duc Thanh <ndt.sparka2003@gmail.com>
2020-10-26 20:39:39 +0700

    a

--- home.js
+++ home.js
@@ -3,42 +3,72 @@ let products = [{
     'price': '10.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-xs-gold-400x460-1-400x460.png',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '5.5',
+    'OS': 'IOS',
+    'Camera': '12MP',
+    'RAM': '3 GB',
+    'ROM': '32GB'
 },
 {
     'name': 'IPHONE 8 PLUS',
     'price': '14.990.000đ',
     'description': 'Giảm thêm 1.000.000đ',
     'img': './img/iphone-8-plus-128gb-082720-052722-400x460.png',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '6.0',
+    'OS': 'IOS',
+    'Camera': '12MP',
+    'RAM': '2 GB',
+    'ROM': '32GB'
 },
 {
     'name': 'IPHONE 7 GOLD',
     'price': '12.990.000đ',
     'description': 'Giảm thêm 1.000.000đ',
     'img': './img/iphone-7-gold-600x600.jpg',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '4.7',
+    'OS': 'IOS',
+    'Camera': '12MP',
+    'RAM': '3 GB',
+    'ROM': '32GB'
 },
 {
     'name': 'IPHONE 8 PLUS 128GB',
     'price': '18.990.000đ',
     'description': 'Giảm thêm 1.000.000đ',
     'img': './img/iphone-8-plus-128gb.jpg',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '4.7',
+    'OS': 'IOS',
+    'Camera': '12MP',
+    'RAM': '3 GB',
+    'ROM': '32GB'
 },
 {
     'name': 'IPHONE 11 PROMAX 512GB',
     'price': '32.990.000đ',
     'description': 'Quà tặng trị giá 4.000.000đ',
     'img': './img/iphone-11-pro-max-512gb-gold-600x600.jpg',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '6.1',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'IPHONE 12 MINI',
     'price': '34.990.000đ',
     'description': 'Quà tặng trị giá 2.000.000đ',
     'img': './img/iphone-12-mini.jpg',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '6.6',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '6 GB',
+    'ROM': '64GB'
 },
 
 {
@@ -46,21 +76,37 @@ let products = [{
     'price': '19.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-11-red-2-400x460-400x460.png',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '6.1',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'IPHONE 11 128GB',
     'price': '21.990.000đ',
     'description': 'khuyễn mãi trị giá 1.000.000đ',
     'img': './img/iphone-11-128gb-green-400x460.png',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '6.1',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '128GB'
 },
 {
     'name': 'IPHONE XS 64GB',
     'price': '17.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-xs-gold-400x460-1-400x460.png',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
+
 },
 
 {
@@ -68,55 +114,90 @@ let products = [{
     'price': '38.990.000đ',
     'description': 'Ưu đãi khủng đến 2 triệu khi đặt sớm',
     'img': './img/iphone-xs-gold-400x460-1-400x460.png',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'IPHONE 12 PROMAX 256GB',
     'price': '38.990.000đ',
     'description': 'Quà tặng trị giá 2.000.000đ',
     'img': './img/iphone-12-pro-max-256gb.jpg',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'IPHONE SE 128GB',
     'price': '33.990.000đ',
     'description': 'Quà tặng trị giá 2.000.000đ',
     'img': './img/iphone-12-pro-max-256gb.jpg',
-    'brand':'IPHONE'
+    'brand':'IPHONE',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'OPPO A12',
     'price': '2.990.000đ',
     'description': 'Giảm 5% khi đặt online',
     'img': './img/oppo-a12-xanh-400x460-400x460.png',
-    'brand':'OPPO'
+    'brand':'OPPO',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
  {
     'name': 'OPPO A31',
     'price': '4.490.000đ',
     'description': 'Quà tặng lên tới 500.000đ',
     'img': './img/oppo-a31-4gb-128gb-trang-400x460-400x460.png',
-    'brand':'OPPO'
+    'brand':'OPPO',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 }, {
     'name': 'OPPO A93',
     'price': '7.490.000đ',
     'description': 'Giảm giá lên tới 5%',
     'img': './img/oppo-a93-230520-060502-400x460.png',
-    'brand':'OPPO'
+    'brand':'OPPO',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 }, 
 {
     'name': 'OPPO A53',
     'price': '4.990.000đ',
     'description': 'Quà tặng trị lên tới 500.000đ',
     'img': './img/oppo-a53-2020-blue-400x460-400x460.png',
-    'brand':'OPPO'
+    'brand':'OPPO',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'OPPO FIND X2',
     'price': '19.990.000đ',
     'description': 'Quà tặng trị lên tới 2.000.000đ',
     'img': './img/oppo-findx.jpg',
-    'brand':'OPPO'
+    'brand':'OPPO',
 },
 {
     'name': 'OPPO RENO 4',
@@ -124,35 +205,59 @@ let products = [{
     'description': 'Quà tặng trị giá lên tới 500.000đ',
     'img': './img/oppo-reno4-260720-040737-400x460.png',
     'imgsub':'./img/oppo-reno4-hot.jpg',
-    'brand':'OPPO'
+    'brand':'OPPO',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'OPPO A9',
     'price': '5.990.000đ',
     'description': 'Giảm giá thêm 5%',
     'img': './img/oppo-a9.jpg',
-    'brand':'OPPO'
+    'brand':'OPPO',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'OPPO A52',
     'price': '5.690.000đ',
     'description': 'Quà tặng trị giá lên tới 500.000đ',
     'img': './img/oppo-A52.jpg',
-    'brand':'OPPO'
+    'brand':'OPPO',    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY A21S',
     'price': '4.390.000Đ',
     'description': 'Quà tặng 300.000đ',
     'img': './img/samsung-galaxy-a21s-3gb-055520-045516-400x460.png',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY A31',
     'price': '6.090.000đ',
     'description': 'Quà tặng 300.000đ',
     'img': './img/samsung-galaxy-a31.png',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY NOTE20 ULTRA',
@@ -160,42 +265,72 @@ let products = [{
     'description': 'Ưu đãi khủng đến 3 triệu khi đặt sớm',
     'img': './img/samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png',
     'imgsub':'./img/samsung-galaxy-note-20-ultra-hot.jpg',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY A71',
     'price': '9.990.000đ',
     'description': 'Giảm giá thêm 500.000đ',
     'img': './img/samsung-galaxy-a71.jpg',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY NOTE20',
     'price': '23.990.000đ',
     'description': 'Giảm giá 3.000.000đ qua quà tặng Galaxy',
     'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY S20 FE',
     'price': '15.990.000đ',
     'description': 'Giảm giá 2.000.000đ qua quà tặng Galaxy',
     'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY A30S',
     'price': '6.290.000đ',
     'description': 'Giảm giá thêm 300.000đ',
     'img': './img/samsung-galaxy-a30s.jpg',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 {
     'name': 'SAMSUNG GALAXY Z-FOLD',
     'price': '50.00.000đ',
     'description': 'Giảm giá 2.000.000đ qua quà tặng Galaxy',
     'img': './img/samsung-galaxy-note-20-xanhla-400x460-400x460.png',
-    'brand':'SAMSUNG'
+    'brand':'SAMSUNG',
+    'screen': '5.8',
+    'OS': 'IOS 13',
+    'Camera': '12MP',
+    'RAM': '4 GB',
+    'ROM': '64GB'
 },
 ]
 
