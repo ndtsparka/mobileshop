af45705498715a9ffc17634d8668d76385b77a75
cuongtq <tranquangcuong97@gmail.com>
2020-10-31 20:00:00 +0700

    ok

---
+++ slide.js
@@ -0,0 +1,23 @@
+let kichthuoc = document.getElementsByClassName('slide')[0].clientWidth;
+let chuyenslide = document.getElementsByClassName('chuyen-slider')[0];
+let chuyen = 0;
+let img = chuyenslide.getElementsByTagName('img');
+var max = kichthuoc*img.length;
+max -=kichthuoc;
+function next(){
+    if(chuyen<max) chuyen += kichthuoc;
+    else chuyen = 0;
+  chuyenslide.style.marginLeft = '-' + chuyen +'px';
+}
+function pre(){
+    if(chuyen === 0) chuyen = max;
+    else
+    chuyen -= kichthuoc;
+    chuyenslide.style.marginLeft = '-' + chuyen +'px';
+
+}
+
+
+setInterval(function(){
+    next();
+},3000);
