747057e57ab49a69e5de1952ebf91d8ad7394806
cuongtq <tranquangcuong97@gmail.com>
2020-10-31 20:17:16 +0700

    ok

--- cart.js
+++ cart.js
@@ -5,7 +5,7 @@ let products = JSON.parse(localStorage.getItem('products'));
 let product = products.find(function (p) {
     return p.name === selectedName;
 });
-
+document.getElementById('phone-name-price')
 document.getElementById('image-preview').src=product.img;
 document.getElementById('tongtien').innerHTML=product.price;
 
@@ -17,3 +17,42 @@ function success(){
 function comehome(){
     window.location.href = 'home.html';
 }
+
+//Tăng giảm số lượng sản phẩm
+//setting default attribute to disabled of minus button
+document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
+
+//taking value to increment decrement input value
+var valueCount
+
+//plus button
+document.querySelector(".plus-btn").addEventListener("click", function() {
+	//getting value of input
+	valueCount = document.getElementById("quantity").value;
+
+	//input value increment by 1
+	valueCount++;
+
+	//setting increment input value
+	document.getElementById("quantity").value = valueCount;
+
+	if (valueCount > 1) {
+		document.querySelector(".minus-btn").removeAttribute("disabled");
+		document.querySelector(".minus-btn").classList.remove("disabled")
+	}
+})
+//minus button
+document.querySelector(".minus-btn").addEventListener("click", function() {
+	//getting value of input
+	valueCount = document.getElementById("quantity").value;
+
+	//input value increment by 1
+	valueCount--;
+
+	//setting increment input value
+	document.getElementById("quantity").value = valueCount
+
+	if (valueCount == 1) {
+		document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+	}
+})
