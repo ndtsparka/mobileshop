40b7a4aa06dfa45819a200144cd963879acb7187
cuongtq <tranquangcuong97@gmail.com>
2020-11-02 15:13:44 +0700

    ok

--- cart.js
+++ cart.js
@@ -6,10 +6,10 @@ let product = products.find(function (p) {
 });
 
 document.getElementById('image-preview').src=product.img;
-document.getElementById('phone-price').innerHTML=product.price + 'đ';
+document.getElementById('phone-price').innerHTML=changeprice(product);
 document.getElementById('phone-name').innerHTML=product.name;
-document.getElementById('total-price').innerHTML=product.price + 'đ';
-document.getElementById('order-value').innerHTML=product.price + 'đ';
+document.getElementById('total-price').innerHTML=changeprice(product);
+document.getElementById('order-value').innerHTML=changeprice(product);
 document.getElementById('desc').innerHTML = `<span>${product.description}</span>`;
 
 function success(e){
@@ -26,7 +26,7 @@ function comehome(){
 document.querySelector(".minus-btn").setAttribute("disabled", "");
 
 //taking value to increment decrement input value
-var valueCount
+var valueCount 
 
 //Lấy giá của sản phẩm
 var price = product.price;
@@ -34,8 +34,8 @@ var price = product.price;
 //Tính tổng giá
 function priceTotal(){
 	var total = valueCount * price;
-	document.getElementById('total-price').innerHTML =total + 'đ';
-	document.getElementById('order-value').innerHTML= total + 'đ';
+	document.getElementById('total-price').innerHTML =totalprice(total);
+	document.getElementById('order-value').innerHTML= totalprice(total);
 }
 // console.log(total);
 //plus button
@@ -75,3 +75,12 @@ document.querySelector(".minus-btn").addEventListener("click", function() {
 	//Tính giá
 	priceTotal();
 })
+
+
+function changeprice(v){
+
+	return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(v.price);
+   }
+function totalprice(v){
+	return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(v);
+}
